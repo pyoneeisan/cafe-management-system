cf79266ba426800037c43a01cb528ce8b30794aa
pyone <pyoneeisan@gmail.com>
2025-04-27 15:03:23 +0700

    Add project files.

---
+++ cafe-management-system/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using cafe_management_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace cafe_management_system.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+        CafeEntities db = new CafeEntities();
+        [HttpPost, Route("signup")]
+        public HttpResponseMessage Signup([FromBody] User user)
+        {
+            try
+            {
+                User userObj = db.Users.FirstOrDefault(u => u.email == user.email);
+                if (userObj != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "User already exists with this email id");
+                }
+                else
+                {
+                    user.status = "true";
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, "User registered successfully");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost, Route("log")]
+        public HttpResponseMessage Login([FromBody] User user)
+        {
+            try
+            {
+                User userObj = db.Users.FirstOrDefault(u => u.email == user.email);
+                if (userObj != null)
+                {
+                    if(userObj.status == "false")
+                    {
+                        return Request.CreateResponse(HttpStatusCode.Forbidden, "Wait for Admin Approval");
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, new { token=TokenManager.GenerateToken(userObj.email,userObj.role)});
+                    }
+                  
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized, "Invalid email or password");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ cafe-management-system/TokenManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace cafe_management_system
+{
+    public class TokenManager
+    {
+        public static string Secret = "sdlfsknvkdosjfwprkslfjsodfiewjfwfjsldfjkfo132432lsdjfoweirwjfoiw";
+        public static string GenerateToken(string email,string role)
+        {
+            // Token generation logic here
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret));
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
